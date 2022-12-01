68ef1851fcbaaecc2c11d9fe192559a18162f249
Hurumnanya <105320491+Hurumnanya@users.noreply.github.com>
2022-12-01 03:52:24 +0100

    completed the task as instructed

---
+++ script.js
@@ -0,0 +1,62 @@
+const change = document.getElementById("boot");
+
+change.addEventListener ("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("car");
+
+change2.addEventListener ("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("gown");
+
+change3.addEventListener ("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("hair");
+
+change4.addEventListener ("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("bag");
+
+change5.addEventListener ("click", (event) => {
+    event.target.style.background = "gold";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("house");
+
+change6.addEventListener ("click", (event) => {
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("jewelleries");
+
+change7.addEventListener ("click", (event) => {
+    event.target.style.background = "white";
+    event.target.style.color = "red";
+});
+
+const change8 = document.getElementById("phone");
+
+change8.addEventListener ("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change9 = document.getElementById("watch");
+
+change9.addEventListener ("click", (event) => {
+    event.target.style.background = "grey";
+    event.target.style.color = "white";
+});
